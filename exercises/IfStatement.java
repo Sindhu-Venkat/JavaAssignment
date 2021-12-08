e75a7cf2aa73887ea2694dd8cfc1634e945cc6cd
Sindhu-Venkat <sindhu.v@busofttech.com>
2021-12-08 12:25:05 +0530

    exercises

---
+++ exercises/IfStatement.java
@@ -0,0 +1,14 @@
+package com.exercises;
+
+public class IfStatement {
+
+
+    public static void main(String[] args) {
+        System.out.println("IF STATEMENT");
+        int age =22;
+        if(age>18){
+            System.out.println("Age is greater than 18");
+        }
+
+    }
+}
---
+++ exercises/IfelseStatement.java
@@ -0,0 +1,18 @@
+package com.exercises;
+
+public class IfelseStatement {
+
+    public static void main(String[] args) {
+
+        System.out.println("IFELSE STATEMENT");
+
+        int year= 2021;
+
+        if(year%4==0){
+            System.out.println("Is a Leap Year");
+        }
+        else{
+            System.out.println("Not a Leap year");
+        }
+    }
+}
