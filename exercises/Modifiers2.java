c9a3f474e1688c4a7d80ea82e23ab8a29894607f
Sindhu-Venkat <sindhu.v@busofttech.com>
2021-12-10 15:54:07 +0530

    exercises

--- exercises/Modifiers2.java
+++ exercises/Modifiers2.java
@@ -1,46 +1,43 @@
 package com.exercises;
 
 class Animal {
-    int animalcount = 2;
+    int Animalcount = 2;
 
     public void animaltypes() {
-        System.out.println("AnimalTypes:" + animalcount);
-
-        class Dog extends Animal {
-            private int doglegs = 4;
-            private void Dog() {
-                System.out.println("Dog Legs:" + doglegs);
-            }
-            void show() {
-                Dog d = new Dog();
-                d.Dog();
-            }
+        System.out.println("AnimalTypes:" + Animalcount);
+    }
+    class Dog {
+        private int doglegs = 4;
+        private void Dog() {
+            System.out.println("Dog Legs:" + doglegs);
         }
-        class Cat extends Animal {
+    }
+    void display() {            //default
+        System.out.println("DEFAULT MODIFIER1");
+        Dog d = new Dog();
+        d.Dog();
+    }
+    class Cat extends Dog{
             private int catlegs = 4;
             private void Cat() {
                 System.out.println("Cat Legs:" + catlegs);
             }
-            void show() {
-                Cat c = new Cat();
-                c.Cat();
-            }
+    }
+    void show(){                //default
+        System.out.println("DEFAULT MODIFIER2");
+        Cat c = new Cat();
+        c.Cat();
+    }
         }
-    }
-
-    void display() {            //default
-        System.out.println("DEFAULT MODIFIER");
-        
-    }
-}
 public class Modifiers2 extends Zoo {
     public static void main(String[] args) {
         System.out.println("ACCESS MODIFIERS");
         Modifiers2 M2 = new Modifiers2();
         M2.Hello();
         Animal a = new Animal();
-        a.display();
         a.animaltypes();
+        a.display();
+        a.show();
 
     }
 }
