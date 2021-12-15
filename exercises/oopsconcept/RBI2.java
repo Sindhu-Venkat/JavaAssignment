6ab67ad31da15aa956da96337804fc139e4d9972
Sindhu-Venkat <sindhu.v@busofttech.com>
2021-12-15 14:27:47 +0530

    exercises

--- exercises/oopsconcept/RBI2.java
+++ exercises/oopsconcept/RBI2.java
@@ -4,8 +4,8 @@ class RBI2 {
     float amount = 1000;
     float RPercentageInterest = 2;
     float timeperiod = 3;
-     float interest() {
-        float RbiInterest = (amount*RPercentageInterest*timeperiod)/100;
+           float interest(float interestrate) {
+        float RbiInterest = interestrate+(amount*RPercentageInterest*timeperiod)/100;
         return RbiInterest;
     }
 }
@@ -13,8 +13,8 @@ class RBI2 {
        float amount = 2000;
        float RPercentageInterest = 3;
        float timeperiod = 4;
-    float interest() {
-        float SbiInterest = (amount*RPercentageInterest*timeperiod)/100;
+             float interest( float interestrate) {
+        float SbiInterest = interestrate+(amount*RPercentageInterest*timeperiod)/100;
         return SbiInterest;
     }
 }
@@ -22,8 +22,8 @@ class RBI2 {
         float amount = 5000;
         float RPercentageInterest = 4;
         float timeperiod = 5;
-        float interest() {
-            float HdfcInterest = (amount*RPercentageInterest*timeperiod)/100;
+             float interest(float interestrate) {
+            float HdfcInterest = interestrate+(amount*RPercentageInterest*timeperiod)/100;
             return  HdfcInterest;
         }
     }
@@ -31,8 +31,8 @@ class RBI2 {
             float amount = 4000;
             float RPercentageInterest = 5;
             float timeperiod = 6;
-            float interest() {
-                float IciciInterest =(amount*RPercentageInterest*timeperiod)/100;
+                  float interest(float interestrate) {
+                float IciciInterest =interestrate +(amount*RPercentageInterest*timeperiod)/100;
                 return IciciInterest;
             }
     public static void main(String args[]) {
@@ -42,13 +42,13 @@ class RBI2 {
         SBI2 S = new SBI2();
         HDFC2 H = new HDFC2();
         ICICI2 I = new ICICI2();
-        float RbiInterest = R.interest()/10;
+        float RbiInterest = R.interest(2)/100;
         System.out.println("RBI INTEREST :"+RbiInterest);
-        float SbiInterest = S.interest()/100;
+        float SbiInterest = S.interest(3)/100;
         System.out.println("SBI INTEREST :"+SbiInterest);
-        float HdfcInterest = H.interest()/100;
+        float HdfcInterest = H.interest(4)/100;
         System.out.println("HDFC INTEREST :"+HdfcInterest);
-        float IciciInterest = I.interest()/1000;
+        float IciciInterest = I.interest(5)/100;
         System.out.println("ICICI INTEREST :"+IciciInterest);
 
         System.out.println("<---------THANK YOU FOR CHOOSING OUR BANK-------->");
