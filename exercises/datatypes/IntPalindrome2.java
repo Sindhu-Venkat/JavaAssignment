26bce8dc8d2703444114a8a189795b578034846d
Sindhu-Venkat <sindhu.v@busofttech.com>
2021-12-28 23:46:38 +0530

    workouts

--- exercises/datatypes/IntPalindrome2.java
+++ exercises/datatypes/IntPalindrome2.java
@@ -5,22 +5,20 @@ package com.exercises.datatypes;
 import java.util.Scanner;
 
 public class IntPalindrome2 {
-
     public static void main(String[] args) {
         System.out.println("<===INTEGER PALINDROME===>");
-        int num,rev=0,rem;     //positive integer // rev - reverse , //rem - remainder
-        Scanner sc = new Scanner(System.in);
-        System.out.println("ENTER THE NUMBER TO BE REVERSED :");
-        num = sc.nextInt();
-        int temp = num;
-        Palindromelogic pl = new Palindromelogic();
-        pl.display(num);
-        if(temp == rev){        //using if else condition checking whether the num is palindrome or not.
-            System.out.println("IS A PALINDROME");
+                int r=0, rem, num;
+                Scanner s = new Scanner(System.in);
+                System.out.print("Enter number that has to be checked:");
+                num = s.nextInt();
+                int temp = num;
+                if (temp == r)
+                {
+                    System.out.println("IS A PALINDROME");
+                }
+                else
+                {
+                    System.out.println("NOT A PALINDROME");
+                }
+            }
         }
-        else{
-            System.out.println("NOT A PALINDROME");
-        }
-    }
-}
-
--- exercises/datatypes/Palindromelogic.java
+++ exercises/datatypes/Palindromelogic.java
@@ -2,20 +2,26 @@ package com.exercises.datatypes;
 
 //Write a Java program to check if a given positive number is a palindrome or not
 
+import java.util.Scanner;
+
 class Palindromelogic {
-    public int display( int num) {
-        int rev = 0, rem;
-        while (num != 0) {
-            rem = num % 10;
-            rev = rem;             // rev = rev * 10 +rem)
-            num = num / 10;
+
+    public int display() {
+
+        int r=0, rem, num;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter number that has to be checked:");
+        num = s.nextInt();
+        int temp = num;
+        while( num != 0 )
+        {
+            rem= num % 10;
+            r= r * 10 + rem;
+            num=num/10;
         }
-        System.out.println("REVERSE RESULT 1 :" + rev);
-        return rev;
+        return r;
     }
 }
-
-
 
 
 
